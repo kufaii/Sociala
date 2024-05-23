5fe618317f85372feed0f404bc80e7e5bd0f93b1
disinikk <72563019+disinikk@users.noreply.github.com>
2024-05-23 12:32:49 +0700

    style: not finished updating home styling

--- my-app/instance.ts
+++ my-app/instance.ts
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "/pake link nghrokkk"
-<<<<<<< HEAD
-  baseURL: "https://05c5-123-253-233-155.ngrok-free.app",
-=======
-  baseURL: "https://15c6-110-138-182-88.ngrok-free.app",
->>>>>>> a1ef223 (style: change profile page style)
+  baseURL: "https://1506-123-253-233-155.ngrok-free.app",
 });
 
 export default instance;
